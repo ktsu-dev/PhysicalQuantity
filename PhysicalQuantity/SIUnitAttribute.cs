89804ee295f0b61a83ed44e7a4ec1bd5b529a18b
matt-edmondson <matthew.edmondson@gmail.com>
2024-06-14 11:24:08 +1000

    Seal SIUnitAttribute class in ktsu.io.PhysicalQuantity
    The SIUnitAttribute class within the ktsu.io.PhysicalQuantity namespace has been updated to be a sealed class, preventing inheritance. The constructor parameters and their respective properties (Symbol, Singular, and Plural) remain as they were, ensuring no change in their initialization process.

--- PhysicalQuantity/SIUnitAttribute.cs
+++ PhysicalQuantity/SIUnitAttribute.cs
@@ -1,7 +1,7 @@
 namespace ktsu.io.PhysicalQuantity;
 
 [AttributeUsage(AttributeTargets.Class)]
-public class SIUnitAttribute(string symbol, string singular, string plural) : Attribute
+public sealed class SIUnitAttribute(string symbol, string singular, string plural) : Attribute
 {
 	public string Symbol { get; init; } = symbol;
 	public string Singular { get; init; } = singular;
