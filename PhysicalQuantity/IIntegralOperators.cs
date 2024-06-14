8f6b8e8d208b011a875d9cc1324bed59fed690d5
matt-edmondson <matthew.edmondson@gmail.com>
2024-06-14 11:50:52 +1000

    Rename IIntegralOperators

--- PhysicalQuantity/IIntegralOperators.cs
+++ PhysicalQuantity/IIntegralOperators.cs
@@ -3,11 +3,11 @@ namespace ktsu.io.PhysicalQuantity;
 using System.Numerics;
 using ktsu.io.PhysicalQuantity.Generic;
 
-public interface IIntegrationOperators<TSelf, TOther, TResult>
+public interface IIntegralOperators<TSelf, TOther, TResult>
 	: IMultiplyOperators<TSelf, TOther, TResult>
-	where TSelf : PhysicalQuantity<TSelf>, IIntegrationOperators<TSelf, TOther, TResult>, new()
+	where TSelf : PhysicalQuantity<TSelf>, IIntegralOperators<TSelf, TOther, TResult>, new()
 	where TOther : PhysicalQuantity<TOther>, new()
 	where TResult : PhysicalQuantity<TResult>, new()
 {
-	public static TResult operator *(IIntegrationOperators<TSelf, TOther, TResult> left, TOther right) => PhysicalQuantity<TSelf>.Multiply<TResult>((TSelf)left, right);
+	public static TResult operator *(IIntegralOperators<TSelf, TOther, TResult> left, TOther right) => PhysicalQuantity<TSelf>.Multiply<TResult>((TSelf)left, right);
 }
